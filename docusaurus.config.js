68c804282e5241d390cb6f7b2543ca890f68681e
Juan-LukeKlopper <jlklopper@protonmail.com>
2023-09-30 17:07:27 +0200

    fix: remove blogs sections and links to them

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -70,7 +70,6 @@ const config = {
             position: 'left',
             label: 'Tutorial',
           },
-          {to: '/blog', label: 'Blog', position: 'left'},
           {
             href: 'https://github.com/facebook/docusaurus',
             label: 'GitHub',
@@ -80,16 +79,7 @@ const config = {
       },
       footer: {
         style: 'dark',
-        links: [
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Tutorial',
-                to: '/docs/intro',
-              },
-            ],
-          },
+        links: [ 
           {
             title: 'Community',
             items: [
@@ -110,10 +100,6 @@ const config = {
           {
             title: 'More',
             items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
               {
                 label: 'GitHub',
                 href: 'https://github.com/facebook/docusaurus',
